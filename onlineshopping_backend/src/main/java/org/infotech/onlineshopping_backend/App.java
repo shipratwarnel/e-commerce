8bdbd693674e0945801698e0b8358ba72c7ddf9d
Shipra Twarneel <shipratwarneel@gmail.com>
2019-10-29 02:12:16 -0500

    My Project setup completed successfully.

---
+++ onlineshopping_backend/src/main/java/org/infotech/onlineshopping_backend/App.java
@@ -0,0 +1,13 @@
+package org.infotech.onlineshopping_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
